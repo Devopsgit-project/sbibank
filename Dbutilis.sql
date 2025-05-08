b97c18deb1fb5838ed1e8a17b6b18c8e40b7015a
krishnadharmavaram <vamsikrishnadharmavaram67@gmail.com>
2025-05-08 11:32:22 +0000

    commit message

---
+++ Dbutilis.sql
@@ -0,0 +1,3 @@
+echo "enter full family details names"
+read family names
+echo "full family names:" $names
---
+++ stringutilis.sh
@@ -0,0 +1,9 @@
+echo "Enter student fullname:"
+read name
+echo "student name:" $name
+echo "Enter your college name:"
+read college name
+echo "college name:"
+echo "Enter your branch:"
+read branch
+echo "branch name:" $branch
